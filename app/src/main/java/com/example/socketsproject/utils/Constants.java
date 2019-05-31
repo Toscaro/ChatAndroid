0df61b45c3cb8ab0a1dadcc2bdcb1fdf26510b6f
Blumer <matpblumer@gmail.com>
2019-05-30 23:59:44 -0300

    criando chat

--- app/src/main/java/com/example/socketsproject/utils/Constants.java
+++ app/src/main/java/com/example/socketsproject/utils/Constants.java
@@ -3,7 +3,7 @@ package com.example.socketsproject.utils;
 public class Constants {
 
     //Don't push your public IP ADDRESS.
-    public static final String IPV4 = "192.168.0.103";
+    public static final String IPV4 = "192.168.0.29";
     //Use your specific PORT number from router.
     public static final int PORT_NUMBER = 1080;
 
